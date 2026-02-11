6821d879e25be8cbb07cb7746a5f51ac39889061
Daisy7942 <zotazoang@gmail.com>
2026-02-11 23:18:02 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -1,9 +1,2 @@
 def solution(num_list):
-    count = 0
-    
-    for x in num_list:
-        while x > 1:
-            x //= 2
-            count += 1
-            
-    return count
+    return sum(len(bin(i)) - 3 for i in num_list)
