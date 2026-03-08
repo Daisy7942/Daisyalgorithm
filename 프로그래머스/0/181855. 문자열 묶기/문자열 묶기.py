5dfe4df63918a4ed57a5bbb9f8c08987ba1050ef
Daisy7942 <zotazoang@gmail.com>
2026-03-08 22:18:55 +0900

    [level 0] Title: 문자열 묶기, Time: 17.36 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,11 +1,3 @@
 def solution(strArr):
-    length_count = {}
-
-    for s in strArr:
-        l = len(s)
-        if l in length_count:
-            length_count[l] += 1
-        else:
-            length_count[l] = 1
-
-    return max(length_count.values())
+    answer = 0
+    return answer
