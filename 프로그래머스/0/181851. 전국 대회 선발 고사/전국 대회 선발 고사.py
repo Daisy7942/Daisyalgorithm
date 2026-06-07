396a8a662fc71a7cf3f0a80b1a025e96d7a1756f
Daisy7942 <zotazoang@gmail.com>
2026-06-07 23:54:26 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -0,0 +1,14 @@
+def solution(rank, attendance):
+    students = []
+
+    for i in range(len(rank)):
+        if attendance[i]:
+            students.append((rank[i], i))
+
+    students.sort()
+
+    a = students[0][1]
+    b = students[1][1]
+    c = students[2][1]
+
+    return 10000 * a + 100 * b + c
