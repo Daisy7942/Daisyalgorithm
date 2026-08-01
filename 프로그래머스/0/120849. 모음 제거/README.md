5ff9d811ec07d06d3bcd5d1aa7a506474c349b30
Daisy7942 <zotazoang@gmail.com>
2026-08-01 23:22:14 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/README.md
+++ 프로그래머스/0/120849. 모음 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 모음 제거 - 120849 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120849) 
 
 ### 성능 요약
 
-메모리: 9.11 MB, 시간: 0.01 ms
+메모리: 11.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 21일 17:34:35
+2026년 08월 01일 23:22:07
 
 ### 문제 설명
 
--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,3 +1,10 @@
 def solution(my_string):
-    vowels = "aeiou"
-    return ''.join([ch for ch in my_string if ch not in vowels])
+    answer = ''
+    for i in range(len(my_string)):
+        if my_string[i]!=('a'):
+            if my_string[i]!=('e'):
+                if my_string[i]!=('i'):
+                    if my_string[i]!=('o'):
+                        if my_string[i]!=('u'):
+                            answer+=my_string[i]          
+    return answer
