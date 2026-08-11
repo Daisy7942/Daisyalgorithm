1c1e47ace4a88147010d03d59096bb4fad593f6b
Daisy7942 <zotazoang@gmail.com>
2026-08-11 23:46:46 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.20 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/README.md
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (1) - 120850 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120850) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120850?language=java) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 0.02 ms
+메모리: 81.6 MB, 시간: 1.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 22일 22:34:09
+2026년 08월 11일 23:46:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        int count = 0;
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) >= '0' && my_string.charAt(i) <= '9') {
+                count++;
+            }
+        }
+
+        int[] answer = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) >= '0' && my_string.charAt(i) <= '9') {
+                answer[index] = my_string.charAt(i) - '0';
+                index++;
+            }
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}
